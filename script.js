a0d731af5cf398666cd55e2297cf0d129cdcf6b6
Ananthakrishnan21 <120053092+Ananthakrishnan21@users.noreply.github.com>
2023-01-10 12:24:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 //your code here
-let typing=document.getElementsByTagName("input")[0]
+
 let word=document.getElementById("evaluatedText")
 let update=document.getElementById("lettercount")
-typing.addEventListener("input",()=>{count(word.value)})
+let btn=document.getElementByTagName("button")[0]
+btn.addEventListener("click",()=>{count(word.value)})
 function count(word){
 	let length=word.length
 	update.innerText=length
