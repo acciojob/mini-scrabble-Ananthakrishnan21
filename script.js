190501860424907ed4eb4c9e7e64fc99a19a066f
Ananthakrishnan21 <120053092+Ananthakrishnan21@users.noreply.github.com>
2023-01-10 12:10:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your code here
+let typing=document.getElementsByTagName("input")[0]
+let word=document.getElementById("evaluatedText")
+let update=document.getElementById("lettercount")
+typing.addEventListener("input",()=>{count(word.value)})
+function count(word){
+	let length=word.length
+	update.innerText=length
+	
+}
+
+
